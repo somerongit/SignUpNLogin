6ba5e053de6874dfffd7fd325793df77b7cae954
somerongit <someron.bakuli05@gmail.com>
2022-01-17 22:17:28 +0530

    Update

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
+
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+const bcrypt = require('bcrypt')
 
+mongoose.connect('mongodb+srv://admin:root@cluster0.kxotw.mongodb.net/DB?retryWrites=true&w=majority')
 const app = express()
-app.use('/',express.static(path.join(__dirname,'static')))
+app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
-app.post('/api/register',async (req,res)=>{
-console.log(req.body);
-res.json({
-    status:'ok'
-})
+app.post('/api/register', async (req, res) => {
+  const { username, password } = req.body
+  console.log(username, await bcrypt.hash(password, 12));
+  try {
+    await User.create({
+      username,
+      password
+    })
+  } catch (err) {
+    console.log(err);
+    return res.json({status:'Error'})
+  }
+  res.json({
+    status: 'ok'
+  })
 })
 
 
-app.listen(3000, ()=>{
-    console.log('Server up at 3000');
+app.listen(3000, () => {
+  console.log('Server up at 3000');
 })
+
+
